13c96df6f65ce23afa03d160c5f8e42508d38857
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2017-07-26 15:20:13 +0200

    Project\DrumKit

---
+++ DrumKit/src/DrumKitTestDriver.java
@@ -0,0 +1,13 @@
+class DrumKitTestDriver{
+
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.snare = false;
+
+        if (d.snare == true){
+            d.playSnare();
+        }
+        d.playSnare();
+        d.playTopHat();
+    }
+}
